15cd3a461442cc40e352f052e3294e6803b5bde6
DIYgod <i@diygod.me>
2022-12-07 21:39:42 +0000

    chore: runtime server env

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
       "default-src 'self'; script-src 'none'; sandbox; style-src 'unsafe-inline';",
     remotePatterns: [{ hostname: "**" }],
   },
+
+  serverRuntimeConfig: {
+    ENV_REDIS_URL: process.env.REDIS_URL,
+    ENV_REDIS_EXPIRE: process.env.REDIS_EXPIRE,
+    ENV_REDIS_REFRESH: process.env.REDIS_REFRESH,
+  },
 }
 
 module.exports = nextConfig
--- src/lib/env.server.ts
+++ src/lib/env.server.ts
@@ -1,4 +1,9 @@
-export const REDIS_URL = process.env.REDIS_URL
+import getConfig from "next/config"
+
+const { ENV_REDIS_URL, ENV_REDIS_EXPIRE, ENV_REDIS_REFRESH } = getConfig()
+
+export const REDIS_URL = ENV_REDIS_URL || process.env.REDIS_URL
 export const REDIS_EXPIRE =
-  parseInt(process.env.REDIS_URL || "0") || 60 * 60 * 24 * 7 // 1 week
-export const REDIS_REFRESH = parseInt(process.env.REDIS_URL || "0") || 5 * 1000 // 5 seconds
+  parseInt(ENV_REDIS_EXPIRE || process.env.REDIS_URL || "0") || 60 * 60 * 24 * 7 // 1 week
+export const REDIS_REFRESH =
+  parseInt(ENV_REDIS_REFRESH || process.env.REDIS_URL || "0") || 5 * 1000 // 5 seconds
