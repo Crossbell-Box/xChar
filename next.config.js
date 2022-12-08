272e6bc08a6d73bc6c082f9f0b7c84cb360f2f66
DIYgod <i@diygod.me>
2022-12-08 00:04:09 +0000

    chore: generateBuildId

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const execSync = require("child_process").execSync
+const lastCommitCommand = "git rev-parse HEAD"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -14,6 +17,10 @@ const nextConfig = {
     ENV_REDIS_URL: process.env.REDIS_URL,
     ENV_REDIS_EXPIRE: process.env.REDIS_EXPIRE,
     ENV_REDIS_REFRESH: process.env.REDIS_REFRESH,
+  },
+
+  async generateBuildId() {
+    return execSync(lastCommitCommand).toString().trim()
   },
 }
 
