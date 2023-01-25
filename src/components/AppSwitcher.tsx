3189907f54d684dfc09c5e857e0eab10e107de4a
DIYgod <i@diygod.me>
2023-01-25 19:39:05 +0000

    feat: add more apps

--- src/components/AppSwitcher.tsx
+++ src/components/AppSwitcher.tsx
@@ -5,6 +5,51 @@ import { XShopIcon } from "~/components/icons/XShop"
 import { XSyncIcon } from "~/components/icons/XSync"
 import { XLogIcon } from "~/components/icons/XLog"
 import { Tooltip } from "~/components/Tooltip"
+import { CrossbellIcon } from "~/components/icons/Crossbell"
+import { RSS3Icon } from "~/components/icons/RSS3Icon"
+
+const apps = [
+  {
+    name: "xChar",
+    icon: <XCharIcon className="w-8 h-8 inline-block" />,
+    url: "https://xchar.app/",
+  },
+  {
+    name: "xFeed",
+    icon: <XFeedIcon className="w-8 h-8 inline-block" />,
+    url: "https://crossbell.io/feed",
+  },
+  {
+    name: "xSync",
+    icon: <XSyncIcon className="w-8 h-8 inline-block" />,
+    url: "https://xsync.app/",
+  },
+  {
+    name: "xShop",
+    icon: <XShopIcon className="w-8 h-8 inline-block" />,
+    text: "Coming soon",
+  },
+  {
+    name: "xLog",
+    icon: <XLogIcon className="w-8 h-8 inline-block" />,
+    url: "https://xlog.app/",
+  },
+  {
+    name: "Scan",
+    icon: <CrossbellIcon className="w-8 h-8 inline-block p-[2px]" />,
+    url: "https://scan.crossbell.io/",
+  },
+  {
+    name: "Faucet",
+    icon: <CrossbellIcon className="w-8 h-8 inline-block p-[2px]" />,
+    url: "https://faucet.crossbell.io/",
+  },
+  {
+    name: "RSS3",
+    icon: <RSS3Icon className="w-8 h-8 inline-block" />,
+    url: "https://rss3.io/",
+  },
+]
 
 export const AppSwitcher = () => {
   return (
@@ -13,62 +58,29 @@ export const AppSwitcher = () => {
         <SquaresIcon className="w-8 h-8 text-zinc-600 fill-current cursor-pointer" />
         <div className="group-hover:block hidden absolute top-full -right-6 pt-3">
           <div className="bg-white rounded-xl ring-1 ring-zinc-100 shadow-md p-2">
-            <ul className="flex space-x-2 text-center text-zinc-600 text-sm">
-              <li>
-                <a
-                  href="https://xchar.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="flex flex-col items-center sm:px-4 px-2 sm:py-2 py-1 hover:bg-zinc-100 rounded-lg"
-                >
-                  <XCharIcon className="w-8 h-8 inline-block" />
-                  <span>xChar</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://crossbell.io/feed"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="flex flex-col items-center sm:px-4 px-2 sm:py-2 py-1 hover:bg-zinc-100 rounded-lg"
-                >
-                  <XFeedIcon className="w-8 h-8 inline-block" />
-                  <span>xFeed</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://xsync.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="flex flex-col items-center sm:px-4 px-2 sm:py-2 py-1 hover:bg-zinc-100 rounded-lg"
-                >
-                  <XSyncIcon className="w-8 h-8 inline-block" />
-                  <span>xSync</span>
-                </a>
-              </li>
-              <li>
-                <Tooltip label="Coming soon" placement="bottom">
-                  <div className="flex flex-col items-center sm:px-4 px-2 sm:py-2 py-1 hover:bg-zinc-100 rounded-lg relative">
-                    <XShopIcon className="w-8 h-8 inline-block" />
-                    <span>xShop</span>
-                  </div>
-                </Tooltip>
-              </li>
-            </ul>
-            <hr className="my-2 -mx-2" />
-            <ul className="flex space-x-2 text-center text-zinc-600 text-sm">
-              <li>
-                <a
-                  href="https://xlog.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="flex flex-col items-center sm:px-4 px-2 sm:py-2 py-1 hover:bg-zinc-100 rounded-lg"
-                >
-                  <XLogIcon className="w-8 h-8 inline-block" />
-                  <span>xLog</span>
-                </a>
-              </li>
+            <ul className="text-center text-zinc-600 text-sm grid grid-cols-4 gap-x-2 gap-y-2 w-72">
+              {apps.map((app) => (
+                <li key={app.name}>
+                  {app.url ? (
+                    <a
+                      href={app.url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="flex flex-col items-center sm:px-4 px-2 sm:py-2 py-1 hover:bg-zinc-100 rounded-lg"
+                    >
+                      {app.icon}
+                      <span>{app.name}</span>
+                    </a>
+                  ) : (
+                    <Tooltip label={app.text!} placement="bottom">
+                      <div className="flex flex-col items-center sm:px-4 px-2 sm:py-2 py-1 hover:bg-zinc-100 rounded-lg relative">
+                        {app.icon}
+                        <span>{app.name}</span>
+                      </div>
+                    </Tooltip>
+                  )}
+                </li>
+              ))}
             </ul>
           </div>
         </div>
